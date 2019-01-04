90ca43c9070158fdbfb7aea5af2a1a625595e460
Anita Graser <anitagraser@gmx.at>
2019-01-04 16:08:43 +0100

    add interactive viz

--- sandbox/summarize_prediction_results.py
+++ sandbox/summarize_prediction_results.py
@@ -45,7 +45,108 @@ elif PREDICTION_MODE == 'kinetic':
     INPUT = 'E:/Geodata/AISDK/predictions_kin.csv' # '/home/agraser/tmp/predictions_kin.csv' 
 
 
-def plot_error_over_future(df):
+def create_interactive_linked_charts(df):
+    df = df[df['future']>=5]
+    #df = df[df['distance_error']<=4000]
+    
+    future_selection = alt.selection_multi(fields=['future'])
+    future_color = alt.condition(future_selection, alt.Color('future:N', legend=None), alt.value('lightgray'))
+    
+    hist = alt.Chart(title='Error Distribution').mark_area(
+            clip=True,
+            opacity=1,
+            interpolate='step'
+        ).encode(
+            alt.X('distance_error', bin=alt.Bin(step=100),  scale=alt.Scale(domain=(0, 4000))),
+            alt.Y('count()', stack=None,  scale=alt.Scale(domain=(0, 600))),#scale=alt.Scale(type='log')),
+            color = future_color 
+        ).add_selection(
+            future_selection
+        ).transform_filter(
+            future_selection
+        )
+    
+    legend = alt.Chart().mark_point().encode(
+            y=alt.Y('future:N', axis=alt.Axis(orient='right')),
+            color=future_color
+        ).add_selection(
+            future_selection
+        )
+        
+    context_selection = alt.selection_multi(fields=['context'])
+    context_color = alt.condition(context_selection, alt.Color('future:N', legend=None), alt.value('lightgray'))
+    map_color = alt.condition(context_selection, alt.value('#4c78a8'), alt.value('#f5f5f5'))
+        
+    scatter = alt.Chart(title='Along-track & Cross-track Error').mark_point(
+            clip=True
+        ).encode(
+            alt.X('along_track_error', scale=alt.Scale(domain=(0, 4000))),
+            alt.Y('cross_track_error', scale=alt.Scale(domain=(0, 4000))),
+            color=context_color,
+            tooltip=['context','future','along_track_error','cross_track_error']
+        ).add_selection(
+            context_selection
+        ).transform_filter(
+            future_selection
+        )
+        
+    countries = alt.topo_feature('https://raw.githubusercontent.com/anitagraser/sandbox/master/land.topojson','land')
+    land = alt.Chart(countries).mark_geoshape(
+            stroke='white',
+            strokeWidth=2
+        ).encode(
+            color=alt.value('lightgray'),
+        ).project(
+            type='equirectangular'            
+        ).properties(
+            width=350,
+            height=230
+        )
+
+    grid = alt.topo_feature('https://raw.githubusercontent.com/anitagraser/sandbox/master/grid.topojson', 'grid')
+    variable_list = ['distance_error', 'along_track_error', 'cross_track_error', 'context', 'future']
+    map_chart = alt.Chart(grid, title='Spatial Distribution').mark_geoshape(
+            stroke='white',
+            strokeWidth=2    
+        ).encode(
+            color=map_color, 
+            tooltip=['context:N']
+        ).transform_lookup(
+            lookup='properties.id',
+            from_=alt.LookupData(df, 'context', variable_list)
+        ).project(
+            type='equirectangular'
+        ).add_selection(
+            context_selection
+        )
+        
+    literature = alt.Chart(df[df['past']==3], title='Literature Comparison').mark_line().encode(
+            x = alt.X('future:Q', 
+                      scale=alt.Scale(domain=(0, 20))),
+            y = alt.Y('mean(distance_error):Q',
+                      scale=alt.Scale(domain=(0,4000))),
+            color=alt.value('#4c78a8')
+        ).transform_filter(
+            context_selection
+        ) 
+    literature = literature + alt.Chart(LITERATURE_ERRORS).mark_point(color='#999').encode(
+            x = alt.X('future'),# ,scale=alt.Scale(domain=(0, 20))),
+            y = alt.Y('error'), # ,scale=alt.Scale(domain=(0,2000)))
+            tooltip=['method','future','error']
+        ).properties(
+            width=350,
+            height=200
+        )   
+        
+    upper = alt.hconcat(scatter, hist, data=df, title='Vessel Trajetory Prediction Errors (in Meters)')
+    lower = alt.hconcat(map_chart+land, legend, data=df)
+    lower = alt.hconcat(lower, literature)
+    chart = alt.vconcat(upper, lower)
+
+    with open(INPUT.replace('.csv','_interactive.vega'),'w') as chart_output:
+        chart_output.write(chart.to_json(indent=2))
+
+def create_error_over_future_graph(df):
     df1 = df.groupby('code').agg({"distance_error": [np.mean, np.std],
                                  "along_track_error": [np.mean, np.std],
                                  "cross_track_error": [np.mean, np.std],
@@ -83,7 +184,9 @@ def plot_error_over_future(df):
     with open(INPUT.replace('.csv','_chart.vega'),'w') as chart_output:
         chart_output.write(chart.to_json(indent=2))
         
-def plot_single_map(df):
+    return chart
+        
+def create_single_map(df):
     df2 = df.groupby('context').mean()
     df2['context'] = df2.index
     print(df2)
@@ -119,7 +222,11 @@ def plot_single_map(df):
     with open(INPUT.replace('.csv','_single_map.vega'),'w') as map_output:
         map_output.write(map_chart.to_json(indent=2))
                 
-def plot_map_series(df):
+def create_map_series(df):
+    # Note that a LayeredChart cannot contain faceted charts as its elements.
+    # Therefore, we cannot use a basemap in this configuration, since it would produce an invalid specification.
+    # More info: https://github.com/altair-viz/altair/issues/785
+    
     df2 = df.groupby('context').mean()
     df2['context'] = df2.index
     print(df2)
@@ -127,21 +234,23 @@ def plot_map_series(df):
     grid = alt.topo_feature('https://raw.githubusercontent.com/anitagraser/sandbox/master/grid.topojson', 'grid')
     variable_list = ['distance_error', 'along_track_error', 'cross_track_error']
     map_chart = alt.Chart(grid).mark_geoshape(
-        stroke='white',
-        strokeWidth=2    
-    ).encode(
-        alt.Color(alt.repeat('row') , type='quantitative')
-    ).transform_lookup(
-        lookup='properties.id',
-        from_=alt.LookupData(df2, 'context', variable_list)
-    ).properties(
-        width=300,
-        height=300
-    ).repeat(
-        row=variable_list
-    ).resolve_scale(
-        color='independent'
-    )
+            stroke='white',
+            strokeWidth=2    
+        ).encode(
+            alt.Color(alt.repeat('row') , type='quantitative')
+        ).transform_lookup(
+            lookup='properties.id',
+            from_=alt.LookupData(df2, 'context', variable_list)
+        ).project(
+            type='equirectangular'
+        ).properties(
+            width=300,
+            height=200
+        ).repeat(
+            row=variable_list
+        ).resolve_scale(
+            color='independent'
+        )
     
     with open(INPUT.replace('.csv','_map_series.vega'),'w') as map_output:
         map_output.write(map_chart.to_json(indent=2))
@@ -159,7 +268,7 @@ if __name__ == '__main__':
             df = pd.read_csv(input_file, ';')
             df['past'] = past
             df['future'] = future
-            df = df[df['future']==5]
+            #df = df[df['future']==5]
             means = df.groupby('context').mean()
             print(means)
             df['code'] = '{}_{}'.format(past,future)
@@ -169,9 +278,11 @@ if __name__ == '__main__':
     df = pd.concat(frames)
     df.to_csv(INPUT.replace('.csv','_summary.csv'))
   
-    plot_error_over_future(df)
-    plot_single_map(df)
-    plot_map_series(df)
+    #create_error_over_future_graph(df)
+    #create_single_map(df)
+    #create_map_series(df)
+    
+    create_interactive_linked_charts(df)
     
     
     print("{} Finished! ...".format(datetime.now()))
