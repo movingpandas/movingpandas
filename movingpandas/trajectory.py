233d427dcf034437c03ed16f47c2bb725b9c0b36
Anita Graser <anitagraser@gmx.at>
2019-07-11 22:33:20 +0200

    Cleanup

--- movingpandas/trajectory.py
+++ movingpandas/trajectory.py
@@ -1,22 +1,5 @@
 # -*- coding: utf-8 -*-
 
-"""
-***************************************************************************
-    trajectory.py
-    ---------------------
-    Date                 : December 2018
-    Copyright            : (C) 2018 by Anita Graser
-    Email                : anitagraser@gmx.at
-***************************************************************************
-*                                                                         *
-*   This program is free software; you can redistribute it and/or modify  *
-*   it under the terms of the GNU General Public License as published by  *
-*   the Free Software Foundation; either version 2 of the License, or     *
-*   (at your option) any later version.                                   *
-*                                                                         *
-***************************************************************************
-"""
-
 import os
 import sys
 
