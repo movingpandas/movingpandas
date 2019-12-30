379e136d33b2cd6ac709cb02c651e1a1c98eca7a
Anita Graser <anitagraser@gmx.at>
2019-12-30 16:37:32 +0100

    Mock geopandas for docs builds

--- docs/setup.py
+++ docs/setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     LONG_DESCRIPTION = fh.read()
 
 # Packages that MovingPandas uses explicitly:
-INSTALL_REQUIRES = ['numpy', 'matplotlib', 'shapely', 'pandas', 'geopandas']
+INSTALL_REQUIRES = ['numpy', 'matplotlib', 'shapely', 'pandas']
 
 setuptools.setup(
     name="movingpandas",
