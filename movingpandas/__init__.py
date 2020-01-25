18703b5bc61bea51d0dd875acb3b0fac8c95a827
Anita Graser <anitagraser@gmx.at>
2020-01-25 16:25:11 +0100

    Add TrajectoryCollectionAggregator

--- movingpandas/__init__.py
+++ movingpandas/__init__.py
@@ -5,6 +5,7 @@
 from .trajectory import Trajectory
 from .trajectory_collection import TrajectoryCollection
 from .trajectory_plotter import TrajectoryPlotter, TrajectoryCollectionPlotter
+from .trajectory_aggregator import TrajectoryCollectionAggregator
 
 name = 'movingpandas'
 __version__ = '0.2.rc2'
