4e9f740428fcb1201ae664c9c9f2f9a2de6fd797
Anita Graser <anitagraser@gmx.at>
2020-07-25 16:19:28 +0200

    Replace fiona.crs with pyproj.CRS

--- movingpandas/tests/test_trajectory_aggregator.py
+++ movingpandas/tests/test_trajectory_aggregator.py
@@ -3,14 +3,14 @@
 import pandas as pd
 from geopandas import GeoDataFrame
 from shapely.geometry import Point
-from fiona.crs import from_epsg
+from pyproj import CRS
 from datetime import datetime, timedelta
 from movingpandas.trajectory_collection import TrajectoryCollection
 from movingpandas.trajectory_aggregator import TrajectoryCollectionAggregator
 
 
-CRS_METRIC = from_epsg(31256)
-CRS_LATLON = from_epsg(4326)
+CRS_METRIC = CRS.from_user_input(31256)
+CRS_LATLON = CRS.from_user_input(4326)
 
 
 class TestTrajectoryCollectionAggregator:
