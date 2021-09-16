a2a4b16d0efde6877f9797783438799378e1623d
Gustavo Pichorim Boiko <gustavo.boiko@gmail.com>
2021-09-16 01:12:44 -0300

    Fix a crash when points are aligned
    If the points are aligned either vertically or horizontally the point
    clusterer was crashing as the number of rows or cols would be zero.

--- movingpandas/tests/test_point_clusterer.py
+++ movingpandas/tests/test_point_clusterer.py
@@ -33,3 +33,27 @@ class TestClustering:
         assert len(actual) == len(expected)
         for pt in expected:
             assert pt in actual
+
+    def test_cluster_horizontal_points(self):
+        pts = [Point(0, 2), Point(0, 6), Point(0, 4), Point(0, 3), Point(0, 5), Point(0, 7)]
+        expected = GeoDataFrame(pd.DataFrame([{'geometry': Point(0, 4.5), 'n': 6}]))
+        actual = PointClusterer(pts, max_distance=5, is_latlon=False).get_clusters()
+        actual = [(c.centroid, len(c.points)) for c in actual]
+        expected = [(c.geometry, c.n) for key, c in expected.iterrows()]
+        assert len(actual) == len(expected)
+        for pt in expected:
+            assert pt in actual
+
+    def test_cluster_vertical_points(self):
+        pts = [Point(0, 2), Point(6, 2), Point(6, 2), Point(0.2, 2), Point(6.2, 2), Point(6.2, 2)]
+        expected = GeoDataFrame(pd.DataFrame([
+            {'geometry': Point(0.1, 2), 'n': 2},
+            {'geometry': Point(6.1, 2), 'n': 4}]
+        ))
+        actual = PointClusterer(pts, max_distance=5, is_latlon=False).get_clusters()
+        actual = [(c.centroid, len(c.points)) for c in actual]
+        expected = [(c.geometry, c.n) for key, c in expected.iterrows()]
+        assert len(actual) == len(expected)
+        print([str(c) for p in actual for c in p])
+        for pt in expected:
+            assert pt in actual
