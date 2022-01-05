44b5a98e4d434b8123dab30d3465d7fbb0c2e4e7
anitagraser <anitagraser@gmx.at>
2022-01-05 18:04:01 +0100

    Add show_version(), fixes #165
    as seen in GeoPandas

--- movingpandas/__init__.py
+++ movingpandas/__init__.py
@@ -23,6 +23,7 @@ from .trajectory_splitter import (  # noqa F401
 )
 from .trajectory_stop_detector import TrajectoryStopDetector  # noqa F401
 from .point_clusterer import PointClusterer  # noqa F401
+from .tools._show_versions import show_versions  # noqa F401
 
 name = "movingpandas"
 __version__ = "0.8.rc1"
