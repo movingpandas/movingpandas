9163e0ff74dfb3574f9cd49f20633d8482efcc71
anitagraser <anitagraser@gmx.at>
2022-01-13 16:24:07 +0100

    Expose import warning to user, fixes #178

--- movingpandas/__init__.py
+++ movingpandas/__init__.py
@@ -28,8 +28,8 @@ from .tools._show_versions import show_versions  # noqa F401
 
 try:
     from .trajectory_smoother import KalmanSmootherCV  # noqa F401
-except ImportError:
-    pass
+except ImportError as e:
+    print(f"INFO: {e}")
 
 name = "movingpandas"
 __version__ = "0.9.rc2"
