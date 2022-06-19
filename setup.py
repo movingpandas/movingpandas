67e593f13bce6cfea01df1f0a6e0bf2c3adf82ab
anitagraser <anitagraser@gmx.at>
2022-06-19 11:56:01 +0200

    Up version to 0.10

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ INSTALL_REQUIRES = [
 
 setuptools.setup(
     name="movingpandas",
-    version="0.9.rc3",
+    version="0.10.rc1",
     author="Anita Graser",
     author_email="anitagraser@gmx.at",
     description="MovingPandas implements Trajectory classes and corresponding methods "
