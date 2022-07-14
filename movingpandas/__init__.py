b4f20805c2fc00331d82d7425ca8038449858c03
Ray Bell <rayjohnbell0@gmail.com>
2022-07-14 00:19:15 -0400

    skip missing optional deps on import

--- movingpandas/__init__.py
+++ movingpandas/__init__.py
@@ -29,8 +29,8 @@ from .tools._show_versions import show_versions  # noqa F401
 
 try:
     from .trajectory_smoother import KalmanSmootherCV  # noqa F401
-except ImportError as e:
-    print(f"INFO: {e}")
+except ImportError:
+    pass
 
 name = "movingpandas"
 __version__ = "0.10.rc1"
