033e2a1a13d5971ee1c64733027e5e09d8434780
anitagraser <anitagraser@gmx.at>
2022-07-23 13:51:01 +0200

    Update trajectory.py
    Speed up to_linestring

--- movingpandas/trajectory.py
+++ movingpandas/trajectory.py
@@ -1043,10 +1043,6 @@ def point_gdf_to_linestring(df, geom_col_name):
     Convert GeoDataFrame of Points to shapely LineString
     """
     if len(df) > 1:
-        return (
-            df.groupby([True] * len(df))[geom_col_name]
-            .apply(lambda x: LineString(x.tolist()))
-            .values[0]
-        )
+        return LineString(df[geom_col_name].tolist())
     else:
         raise RuntimeError("DataFrame needs at least two points to make line!")
