5d348ff0795efc753a9585abfed1f49a4f20774c
Anita Graser <anitagraser@gmx.at>
2023-04-04 16:04:21 +0200

    Update test_trajectory_collection.py

--- movingpandas/tests/test_trajectory_collection.py
+++ movingpandas/tests/test_trajectory_collection.py
@@ -2,7 +2,7 @@
 
 import pandas as pd
 import pytest
-from pandas.util.testing import assert_frame_equal
+from pandas.testing import assert_frame_equal
 from geopandas import GeoDataFrame
 from shapely.geometry import Point, Polygon, LineString
 from fiona.crs import from_epsg
