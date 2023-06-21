b7fa5cc856d8bf5bcef7874f56b2fa84b642cfb4
Anita Graser <anitagraser@gmx.at>
2023-06-21 23:56:36 +0200

    Faster stop detection (#316), fixes #294
    * Update trajectory_stop_detector.py
    
    * Up version to 0.16.1
    
    * Update trajectory_stop_detector.py
    
    * Added _process_traj_collection_multithreaded
    
    * set n_thread to default to 1
    
    * try to trigger tests on all PRs to any branch
    
    * try leaving pull_requests selection empty
    
    ---------
    
    Co-authored-by: Ray Bell <rayjohnbell0@gmail.com>

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ INSTALL_REQUIRES = [
 
 setuptools.setup(
     name="movingpandas",
-    version="0.16.0",
+    version="0.16.1",
     author="Anita Graser",
     author_email="anitagraser@gmx.at",
     description="MovingPandas implements Trajectory classes and corresponding methods "
