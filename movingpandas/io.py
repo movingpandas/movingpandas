c3ac3736b7daa08a7c6b57bf7075fe958504d2ee
Gaspard Merten <gaspard.mp.work@gmail.com>
2023-07-21 17:50:40 +0200

    Adding the ability to parse MovingFeatureCollection using read_mf_json

--- movingpandas/io.py
+++ movingpandas/io.py
@@ -4,7 +4,146 @@ from typing import Callable, Dict
 from geopandas import GeoDataFrame
 from pandas import DataFrame
 
-from movingpandas import Trajectory
+from movingpandas import Trajectory, TrajectoryCollection
+
+
+def gdf_to_mf_json(
+    gdf: GeoDataFrame,
+    traj_id_column: str,
+    datetime_column: str,
+    temporal_columns: list = None,
+    temporal_columns_static_fields: Dict[str, Dict] = None,
+    interpolation: str = None,
+    crs=None,
+    trs=None,
+    datetime_encoder: Callable[[any], str | int] = None,
+) -> dict:
+    """
+    Converts a GeoDataFrame to a dictionary compatible with the Moving Features JSON
+    (MF-JSON) specification.
+
+    Args:
+        gdf (GeoDataFrame): The input GeoDataFrame to convert.
+        traj_id_column (str): The name of the column in the GeoDataFrame that
+            represents the trajectory identifier.
+        datetime_column (str): The name of the column in the GeoDataFrame that
+            represents the datetime information.
+        temporal_columns (list, optional): A list of column names in the GeoDataFrame
+         that represent additional temporal properties. Defaults to None.
+        temporal_columns_static_fields (Dict[str, Dict], optional): A dictionary mapping
+            column names to static fields associated with the corresponding temporal
+            property. Defaults to None.
+        interpolation (str, optional): The interpolation method used for the temporal
+            geometry. Defaults to None.
+        crs (optional): Coordinate reference system for the MF-JSON. Defaults to None.
+        trs (optional): Temporal reference system for the MF-JSON. Defaults to None.
+        datetime_encoder (Callable[[any], str|int], optional): A function that encodes
+            the datetime values in the GeoDataFrame to a string ( IETF RFC 3339 ) or
+            integer ( Timestamp, milliseconds ). Defaults to None.
+    Returns:
+        dict: The MF-JSON representation of the GeoDataFrame as a dictionary.
+    """
+
+    _raise_error_if_invalid_arguments(gdf, datetime_column, traj_id_column)
+
+    if not temporal_columns:
+        temporal_columns = []
+
+    rows = []
+
+    for identifier, row in gdf.groupby(traj_id_column):
+        datetimes = _retrieve_datetimes_from_row(datetime_column, datetime_encoder, row)
+
+        properties = row.drop(
+            columns=[
+                "geometry",
+                datetime_column,
+                traj_id_column,
+                *temporal_columns,
+            ]
+        )
+
+        if properties.empty:
+            encoded_properties = {}
+        else:
+            encoded_properties = properties.to_dict(orient="records")[0]
+
+        trajectory_data = {
+            "type": "Feature",
+            "properties": {
+                traj_id_column: identifier,
+                **encoded_properties,
+            },
+            "temporalGeometry": {
+                "type": "MovingPoint",
+                "coordinates": list(zip(row.geometry.x, row.geometry.y)),
+                "datetimes": datetimes,
+            },
+        }
+
+        if interpolation:
+            trajectory_data["temporalGeometry"]["interpolation"] = interpolation
+
+        if crs:
+            trajectory_data["crs"] = crs
+
+        if trs:
+            trajectory_data["trs"] = trs
+
+        if temporal_columns:
+            temporal_properties_data = _encode_temporal_properties(
+                datetimes, row, temporal_columns, temporal_columns_static_fields
+            )
+
+            trajectory_data["temporalProperties"] = [temporal_properties_data]
+
+        # Appending each trajectory data to the list of rows
+        rows.append(trajectory_data)
+
+    return {"type": "FeatureCollection", "features": rows}
+
+
+def _raise_error_if_invalid_arguments(
+    gdf: GeoDataFrame, datetime_column: str, traj_id_property: str
+):
+    if not isinstance(gdf, GeoDataFrame):
+        raise TypeError(
+            "Not a GeoDataFrame, but a {} was supplied. This function only works with"
+            " GeoDataFrames.".format(type(gdf))
+        )
+    # Check if both datetime_column and trip_id_property are in the GeoDataFrame
+    if datetime_column not in gdf.columns:
+        raise ValueError(
+            f"The datetime_column {datetime_column} is not in the GeoDataFrame."
+        )
+    if traj_id_property not in gdf.columns:
+        raise ValueError(
+            f"The traj_id_property {traj_id_property} is not in the GeoDataFrame."
+        )
+
+
+def _retrieve_datetimes_from_row(datetime_column, datetime_encoder, row):
+    datetimes = row[datetime_column].tolist()
+    if datetime_encoder:
+        datetimes = [datetime_encoder(dt) for dt in datetimes]
+    return datetimes
+
+
+def _encode_temporal_properties(
+    datetimes, row, temporal_properties, temporal_properties_static_fields
+):
+    temporal_properties_data = {
+        "datetimes": datetimes,
+    }
+    for prop in temporal_properties:
+        temporal_properties_data[prop] = {
+            "values": row[prop].tolist(),
+        }
+        if prop in (temporal_properties_static_fields or {}):
+            temporal_properties_data[prop].update(
+                temporal_properties_static_fields[prop]
+            )
+    return temporal_properties_data
 
 
 def gdf_to_mf_json(
@@ -174,12 +313,12 @@ def read_mf_json(json_file_path, traj_id_property=None, traj_id=0):
 def _create_objects_from_mf_json_dict(data, traj_id_property=None, traj_id=0):
     if not isinstance(data, dict):
         raise ValueError("Not a supported MovingFeatures JSON")
-    if not ("type" in data and "temporalGeometry" in data):
-        raise ValueError("Not a supported MovingFeatures JSON")
-    if data["type"] == "Feature":
+    if data["type"] == "Feature" and "temporalGeometry" in data:
         return _create_traj_from_movingfeature_json(data, traj_id_property, traj_id)
-    elif data["type"] == "FeatureCollection":
-        return _create_trajcollection_from_movingfeaturecollection_json(data)
+    elif data["type"] == "FeatureCollection" and "features" in data:
+        return _create_trajcollection_from_movingfeaturecollection_json(
+            data, traj_id_property
+        )
     else:
         raise ValueError(
             f"Not a supported MovingFeatures JSON: "
@@ -236,5 +375,15 @@ def _create_traj_from_movingfeature_json(data, traj_id_property, traj_id):
     return Trajectory(df, traj_id, t="t", x="x", y="y")
 
 
-def _create_trajcollection_from_movingfeaturecollection_json(data):
-    raise RuntimeError("MovingFeatureCollection support is not available yet.")
+def _create_trajcollection_from_movingfeaturecollection_json(data, traj_id_property):
+    assert (
+        traj_id_property is not None
+    ), "traj_id_property must be supplied when reading a collection of trajectories"
+
+    trajectories = []
+    for feature in data["features"]:
+        trajectories.append(
+            _create_traj_from_movingfeature_json(feature, traj_id_property, None)
+        )
+
+    return TrajectoryCollection(trajectories)
