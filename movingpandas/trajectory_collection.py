ff24b9ad9ca0fc5f82a7e4da9b9343aafa8d0d9a
anitagraser <anitagraser@gmx.at>
2024-07-06 19:52:28 +0200

    Support temporal properties, fixes #408

--- movingpandas/trajectory_collection.py
+++ movingpandas/trajectory_collection.py
@@ -180,7 +180,7 @@ class TrajectoryCollection:
         gdf.reset_index(drop=True, inplace=True)
         return gdf
 
-    def to_mf_json(self, datetime_to_str=True):
+    def to_mf_json(self, datetime_to_str=True, temporal_columns=None):
         """
         Converts a TrajectoryCollection to a dictionary compatible with the Moving
         Features JSON (MF-JSON) specification.
@@ -194,12 +194,14 @@ class TrajectoryCollection:
             dict: The MF-JSON representation of the GeoDataFrame as a dictionary.
         """
         tmp = self.to_point_gdf()
+        print(tmp)
         t = tmp.index.name
         mf_json = gdf_to_mf_json(
             tmp.reset_index(),
             self.get_traj_id_col(),
             t,
             datetime_to_str=datetime_to_str,
+            temporal_columns=temporal_columns
         )
         return mf_json
 
