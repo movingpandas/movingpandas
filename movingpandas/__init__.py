c9663d4a1d65fd7f494692e4ac76151a14282fb3
SiggyF <f.baart@gmail.com>
2025-04-21 11:46:30 +0200

    moved noqa hint back

--- movingpandas/__init__.py
+++ movingpandas/__init__.py
@@ -32,8 +32,8 @@ from .trajectory_splitter import (  # noqa F401
 from .trajectory_cleaner import IqrCleaner, OutlierCleaner  # noqa F401
 from .trajectory_stop_detector import TrajectoryStopDetector  # noqa F401
 from .point_clusterer import PointClusterer  # noqa F401
-from .cpa import CPACalculator  # noqa F401
-from .tools._show_versions import show_versions
+from .cpa import CPACalculator
+from .tools._show_versions import show_versions  # noqa F401
 
 try:
     from .trajectory_smoother import KalmanSmootherCV  # noqa F401
