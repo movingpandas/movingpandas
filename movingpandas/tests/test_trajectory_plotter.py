aa349003a550e090ecfa1853ab77d36bf8675fa2
Anita Graser <anitagraser@gmx.at>
2025-05-11 21:05:18 +0200

    Add lazy geom col tests to plotter

--- movingpandas/tests/test_trajectory_plotter.py
+++ movingpandas/tests/test_trajectory_plotter.py
@@ -1,10 +1,11 @@
 # -*- coding: utf-8 -*-
 
-import pandas as pd
+from pandas import DataFrame
 from geopandas import GeoDataFrame
 from shapely.geometry import Point
 from pyproj import CRS
-from datetime import datetime
+from datetime import datetime, timedelta
+from movingpandas.trajectory import Trajectory
 from movingpandas.trajectory_collection import TrajectoryCollection
 from movingpandas.trajectory_plotter import _TrajectoryPlotter
 
@@ -14,7 +15,7 @@ CRS_LATLON = CRS.from_user_input(4326)
 
 class TestTrajectoryCollection:
     def setup_method(self):
-        df = pd.DataFrame(
+        df = DataFrame(
             [
                 [1, "A", Point(0, 0), datetime(2018, 1, 1, 12, 0, 0), 9, "a"],
                 [1, "A", Point(6, 0), datetime(2018, 1, 1, 12, 0, 6), 5, "b"],
@@ -35,3 +36,37 @@ class TestTrajectoryCollection:
         min_value, max_value = self.plotter.get_min_max_values()
         assert min_value == 2
         assert max_value == 10
+
+class TestTrajectoryCollectionNonGeo:
+    def setup_method(self):
+        n = 3
+        start = datetime(2023, 1, 1)
+        data = {
+            "t": [start + timedelta(seconds=i) for i in range(n)],
+            "x": [0, 1, 2],
+            "y": [0, 0, 0],
+            "val": [2, 5, 10]
+        }
+        df = DataFrame(data)
+        self.traj_nongeo_xyt = Trajectory(df, traj_id=1, t="t", x="x", y="y", crs=CRS_METRIC)
+
+        data = {
+            "a": [start + timedelta(seconds=i) for i in range(n)],
+            "b": [0, 1, 2],
+            "c": [0, 0, 0],
+            "val": [2, 5, 10]
+        }
+        df = DataFrame(data)
+        self.traj_nongeo_abc = Trajectory(df, traj_id=1, t="a", x="b", y="c", crs=CRS_METRIC)
+
+    def test_get_min_max_values_nongeo(self):
+        self.plotter = _TrajectoryPlotter(self.traj_nongeo_xyt, column="val")
+        min_value, max_value = self.plotter.get_min_max_values()
+        assert min_value == 2
+        assert max_value == 10
+
+    def test_get_min_max_values_nongeo_custom_cols(self):
+        self.plotter = _TrajectoryPlotter(self.traj_nongeo_abc, column="val")
+        min_value, max_value = self.plotter.get_min_max_values()
+        assert min_value == 2
+        assert max_value == 10        
